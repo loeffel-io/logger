e29a17d0ce6db94b3cd935f561071e2b0339b62d
Lucas Löffel <lucas@loeffel.io>
2019-10-16 15:43:00 +0200

    add mutex

--- logger.go
+++ logger.go
@@ -3,10 +3,26 @@ package logger
 import (
 	"github.com/getsentry/sentry-go"
 	log "github.com/sirupsen/logrus"
+	"sync"
 )
 
 type Logger struct {
 	Debug bool
+	*sync.RWMutex
+}
+
+func (logger *Logger) GetDebug() bool {
+	logger.RLock()
+	defer logger.RUnlock()
+
+	return logger.Debug
+}
+
+func (logger *Logger) SetDebug(debug bool) {
+	logger.Lock()
+	defer logger.Unlock()
+
+	logger.Debug = debug
 }
 
 func (logger *Logger) Error(err error) {
@@ -15,7 +31,7 @@ func (logger *Logger) Error(err error) {
 }
 
 func (logger *Logger) Log(err error) {
-	if !logger.Debug {
+	if !logger.GetDebug() {
 		return
 	}
 
@@ -24,7 +40,7 @@ func (logger *Logger) Log(err error) {
 }
 
 func (logger *Logger) Print(msg string) {
-	if !logger.Debug {
+	if !logger.GetDebug() {
 		return
 	}
 
